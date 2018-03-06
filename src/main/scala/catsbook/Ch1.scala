9f4504d554f151dffab4215cbaf21849ca6fd98e
joseph.kim <joseph.kim@workday.com>
2018-03-05 21:10:22 -0800

    Fixed Ch1

--- src/main/scala/catsbook/Ch1.scala
+++ src/main/scala/catsbook/Ch1.scala
@@ -80,15 +80,32 @@ object Ch1 {
   }
 
   object Section5 {
-    import cats.Eq
-    import cats.syntax.eq._
-    import cats.instances.int._
-    import cats.instances.string._
+    def run() {
+      import cats.Eq
+      import cats.syntax.eq._
+      import cats.instances.int._
+      import cats.instances.string._
 
-    implicit val catEq: Eq[Cat] = Eq.instance[Cat] { (cat1, cat2) =>
-      (cat1.name === cat2.name) &&
-        (cat1.age === cat2.age) &&
-        (cat1.color === cat2.color)
+      implicit val catEq: Eq[Cat] = Eq.instance[Cat] { (cat1, cat2) =>
+        (cat1.name === cat2.name) &&
+          (cat1.age === cat2.age) &&
+          (cat1.color === cat2.color)
+      }
+
+      val cat1 = Cat("Garfield",   38, "orange and black")
+      val cat2 = Cat("Heathcliff", 33, "orange and black")
+      println(thor =!= loki)
+      println(cat1 === cat1)
+      println(cat1 =!= cat2)
+
+      // Implicit collision with scalatest, so test this here
+      import cats.instances.option._
+
+      val optionCat1 = Option(cat1)
+      val optionCat2 = Option.empty[Cat]
+      println(optionCat1 =!= optionCat2)
+      println(optionCat1 === Some(cat1))
+      println(optionCat2 === optionCat2)
     }
   }
 
--- src/test/scala/catsbook/unit/Ch1Spec.scala
+++ src/test/scala/catsbook/unit/Ch1Spec.scala
@@ -18,19 +18,7 @@ class Ch1Spec extends FlatSpec with Matchers {
   }
 
   it should "Section5" in {
-    import Section5._
-    import cats.syntax.option._
-
-    val cat1 = Cat("Garfield",   38, "orange and black")
-    val cat2 = Cat("Heathcliff", 33, "orange and black")
-    val optionCat1 = Option(cat1)
-    val optionCat2 = Option.empty[Cat]
-
-    (thor =!= loki) shouldEqual true
-    (cat1 === cat1) shouldEqual true
-    (cat1 =!= cat2) shouldEqual true
-    (optionCat1 =!= optionCat2) shouldEqual true
-    (optionCat1 === Some(cat1)) shouldEqual true
-    (optionCat2 === optionCat2) shouldEqual true
+    // val convertToEqualizer = ()  // shadow ScalaTest
+    Section5.run
   }
 }
